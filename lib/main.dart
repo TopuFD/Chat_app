cf02b00e18ce5d674a945f22be7206ff93aed6ef
TopuFD <topuroy3317@gmail.com>
2024-02-09 19:45:46 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:my_chat/view/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+            appBarTheme: AppBarTheme(
+                elevation: 5,
+                backgroundColor: Colors.white,
+                centerTitle: true,
+                titleTextStyle: TextStyle(
+                  fontSize: 20.sp,
+                  color: Colors.black,
+                  fontWeight: FontWeight.normal
+                )),
+            useMaterial3: true,
+          ),
+          home: child,
+        );
+      },
+      child: const HomeScreen(),
+    );
+  }
+}
---
+++ lib/view/home_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Chat App"),
+        leading: IconButton(onPressed: () {}, icon: Icon(Icons.home)),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
+        ],
+      ),
+      floatingActionButton: CircleAvatar(
+        backgroundColor: Colors.blue,
+          child: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.add_comment_rounded,
+                color: Colors.white,
+              ))),
+    );
+  }
+}
