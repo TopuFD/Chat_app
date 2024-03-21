f3937fa6833a98f7a16d4f3a420554307a2dd733
TopuFD <topuroy3317@gmail.com>
2024-03-22 00:15:12 +0600

    add splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:my_chat/view/auth/login_screen.dart';
+import 'package:my_chat/view/auth/splash_screen.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -38,7 +38,7 @@ class MyApp extends StatelessWidget {
           home: child,
         );
       },
-      child: const LoginScreen(),
+      child: const Splash_screen(),
     );
   }
 }
---
+++ lib/view/auth/splash_screen.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:lottie/lottie.dart';
+import 'package:my_chat/view/auth/login_screen.dart';
+
+class Splash_screen extends StatelessWidget {
+  const Splash_screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Timer(Duration(seconds: 4), () {
+      Navigator.push(context, MaterialPageRoute(builder: (_) => LoginScreen()));
+    });
+    return SafeArea(
+        child: Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Lottie.asset("images/splash_screen.json", width: 400.w),
+          Text(
+            "Lodding...",
+            style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.normal,
+                color: Color.fromARGB(255, 89, 0, 255)),
+          )
+        ],
+      ),
+    ));
+  }
+}
